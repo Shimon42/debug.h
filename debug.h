7e41438e3e32ae12eed433f5fbfdefff6c9b9cd7
Siméon F <55737287+Shimon42@users.noreply.github.com>
2019-12-09 19:25:59 +0100

    Add files via upload

---
+++ debug.h
@@ -0,0 +1,45 @@
+#define DEBUG  0
+
+// --------------- COLORS --
+#define RST "\033[0m"
+
+#define RED		"\033[0;31m"
+#define GRN		"\033[0;32m"
+#define YELO	"\033[0;33m"
+#define BLUE 	"\033[0;34m"
+#define PINK	"\033[0;35m"
+#define CYAN	"\033[0;36m"
+#define GREY	"\033[0;37m"
+
+#define BRED	"\033[1;31m"
+#define BGRN	"\033[1;32m"
+#define BYELO	"\033[1;33m"
+#define BBLUE 	"\033[1;34m"
+#define BPINK	"\033[1;35m"
+#define BCYAN	"\033[1;36m"
+#define BGREY	"\033[1;37m"
+
+#define UWHT	"\033[4;1m"
+#define URED	"\033[4;31m"
+#define UGRN	"\033[4;32m"
+#define UYELO	"\033[4;33m"
+#define UBLUE	"\033[4;34m"
+#define UPINK	"\033[4;35m"
+#define UCYAN	"\033[4;36m"
+#define UGREY	"\033[4;37m"
+
+#define BGWHT	"\033[7;1m"
+#define BGRED	"\033[7;31m"
+#define BGGRN	"\033[7;32m"
+#define BGYELO	"\033[7;33m"
+#define BGBLUE	"\033[7;34m"
+#define BGPINK	"\033[7;35m"
+#define BGCYAN	"\033[7;36m"
+#define BGGREY	"\033[7;37m"
+
+// --------------- EDIT
+
+#define TAB		"\t"
+#define DTAB	"\t\t"
+#define TTAB	"\t\t\t"
+#define QTAB	"\t\t\t\t"
